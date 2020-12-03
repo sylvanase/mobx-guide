4a40a48e16accc53ca035fb9249c37170f38d9c8
sylvanas <jzm362466464@126.com>
2020-12-03 23:34:52 +0800

    异步更新

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import { inject, observer } from 'mobx-react'
 // 将组件变成观察者
 @observer
 class App extends Component {
+	componentDidMount() {
+		// const { getData } = this.props.counter
+		// getData()
+	}
 	render() {
 		const { counter } = this.props
+		console.log(this.props)
 		return (
 			<div>
 				<button onClick={counter.increment}>+</button>
-				<button onClick={counter.decrement}>-</button>
 				<span>{counter.count}</span>
+				<button onClick={counter.decrement}>-</button>
+				<span>{counter.getResult}</span>
+				{/* <div>
+					{counter.users.map((user) => (
+						<div>
+							<span>{user.id}</span>
+							<span>{user.login}</span>
+						</div>
+					))}
+				</div> */}
 			</div>
 		)
 	}
--- src/stores/counterStore.js
+++ src/stores/counterStore.js
@@ -2,20 +2,40 @@
  * @Author: sylvanas
  * @Date: 2020-12-02 22:36:42
  * @LastEditors: sylvanas
- * @LastEditTime: 2020-12-02 22:55:02
+ * @LastEditTime: 2020-12-03 23:33:44
  * @Description:
  */
-import { observer } from 'mobx'
+import { observable, action, runInAction, flow, computed } from 'mobx'
+import axios from 'axios'
 class CounterStore {
 	// 将count 变为可观察状态
-	@observer count = 0
+	@observable count = 0
+	@observable users = []
 
-	increment = () => {
+	@action.bound increment() {
 		this.count = this.count + 1
 	}
-
+	// 普通方式
 	decrement = () => {
 		this.count = this.count - 1
+	}
+
+	// 方式一：异步更新
+	@action.bound async getData() {
+		let { data } = await axios.get('https://api.github.com/users')
+		console.log(data)
+		// 严格模式下异步无法直接修改数据，需要使用 runInAction
+		runInAction(() => (this.users = data))
+	}
+
+	// 方式二：异步更新
+	// getData = flow(function* () {
+	//   let { data } = yield axios.get('https://api.github.com/users');
+	//   this.users = data
+	// }).bind(this)
+
+	@computed get getResult() {
+		return this.count * 10
 	}
 }
 
